5e5d20ddefbe6f600e13d81dc5f8f0df995998e6
Muhammad Haseeb <haseebn2868@gmail.com>
2023-08-17 15:20:13 +0500

    Fifth commit

--- src/components/shared/BasePath.ts
+++ src/components/shared/BasePath.ts
@@ -1,3 +1,5 @@
 const BASE_PATH_FOR_API =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "";
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://dine-market-pm5bj87m0-haseeb0128.vercel.app";
 export default BASE_PATH_FOR_API;
