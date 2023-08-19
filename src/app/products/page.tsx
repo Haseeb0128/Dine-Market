8c8f86261b4a99f4e247fac61eeeed9818f26f50
Muhammad Haseeb <haseebn2868@gmail.com>
2023-08-19 11:31:11 +0500

    Eigth commit

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -23,9 +23,10 @@ const Products = async () => {
     <div className="grid lg:grid-cols-4 sm:grid-cols-2 gap-10 px-4 py-2 mt-20 mb-20">
       {productData.productArray.map((item: oneProductType, index: number) => {
         return (
-          <Link href={`/product/${item.slug.current}`}>
-            <Card key={index} singleProductData={item} />
-          </Link>
+          // <Link href={`/product/${item.slug.current}`}>
+          //   <Card key={index} singleProductData={item} />
+          // </Link>
+          <div>Hello</div>
         );
       })}
     </div>
