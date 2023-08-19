c123f022d29da3eb2e46feb2f666107a030af12c
Muhammad Haseeb <haseebn2868@gmail.com>
2023-08-19 13:14:37 +0500

    Firstv2

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,21 +1,30 @@
-import BASE_PATH_FORAPI from "@/components/shared/BasePath";
-import AllProductsCompo from "@/components/views/AllProducts";
+// import BASE_PATH_FOR_API from "@/components/shared/BasePath";
+import { responseType } from "@/components/utils/ProductsDataArrayAndType";
+import { AllProductsCompo } from "@/components/views/AllProducts";
+// import { Card } from "@/components/views/Card";
+// import Link from "next/link";
 
 async function fetchAllProductData() {
-  let res = await fetch(`${BASE_PATH_FORAPI}/api/products?start=0&end=16`, {
-    next: {
-      revalidate: 120,
-    },
-  });
+  let res = await fetch(
+    `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2023-08-08/data/query/production?query=*%5B_type+%3D%3D+%22products%22%5D`,
+    {
+      next: {
+        revalidate: 120,
+      },
+    }
+  );
+
   if (!res.ok) {
-    throw new Error("Failed to fetch");
+    throw new Error("Failed to fetch data");
   }
   return res.json();
 }
 
 const Products = async () => {
-  const ProdutcData = await fetchAllProductData();
-  return <AllProductsCompo ProdutcData={ProdutcData} />;
+  let { result }: responseType = await fetchAllProductData();
+  // console.log(productData);
+
+  return <AllProductsCompo ProductData={result} />;
 };
 
 export default Products;
