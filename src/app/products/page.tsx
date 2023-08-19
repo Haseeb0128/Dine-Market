3c712438f19359d3745d6ee03075f5843f746555
Muhammad Haseeb <haseebn2868@gmail.com>
2023-08-19 12:11:20 +0500

    Ninth Commit

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,36 +1,21 @@
-import BASE_PATH_FOR_API from "@/components/shared/BasePath";
-import { oneProductType } from "@/components/utils/ProductsDataArrayAndType";
-import { Card } from "@/components/views/Card";
-import Link from "next/link";
+import BASE_PATH_FORAPI from "@/components/shared/BasePath";
+import AllProductsCompo from "@/components/views/AllProducts";
 
 async function fetchAllProductData() {
-  let res = await fetch(`${BASE_PATH_FOR_API}/api/products?start=0&end=16`, {
+  let res = await fetch(`${BASE_PATH_FORAPI}/api/products?start=0&end=16`, {
     next: {
       revalidate: 120,
     },
   });
   if (!res.ok) {
-    throw new Error("Failed to fetch data");
+    throw new Error("Failed to fetch");
   }
   return res.json();
 }
 
 const Products = async () => {
-  const productData = await fetchAllProductData();
-  // console.log(productData);
-
-  return (
-    <div className="grid lg:grid-cols-4 sm:grid-cols-2 gap-10 px-4 py-2 mt-20 mb-20">
-      {productData.productArray.map((item: oneProductType, index: number) => {
-        return (
-          // <Link href={`/product/${item.slug.current}`}>
-          //   <Card key={index} singleProductData={item} />
-          // </Link>
-          <div>Hello</div>
-        );
-      })}
-    </div>
-  );
+  const ProdutcData = await fetchAllProductData();
+  return <AllProductsCompo ProdutcData={ProdutcData} />;
 };
 
 export default Products;
